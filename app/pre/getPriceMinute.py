be6326a5e262a1b5a4e0d5cb8f96faefb642d350
sil <46750682+siwei-li@users.noreply.github.com>
2021-12-01 14:16:28 -0800

    Deleted app draft folder

--- app/pre/getPriceMinute.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get(
-        "https://eodhistoricaldata.com/api/intraday/AAPL.US?api_token=OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX&interval=1h\
-        &from=1569913200&to=1577865600"
-    )
